20c6d1f742655a1616fc46f29c15eca90922fc04
Sean Wong <seanwong@Seans-MacBook-Pro.local>
2026-06-19 01:58:27 +0800

    Fix CORS to allow DELETE method

--- worker/index.js
+++ worker/index.js
@@ -1,6 +1,6 @@
 const CORS = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Methods": "GET, POST, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type",
 };
 
